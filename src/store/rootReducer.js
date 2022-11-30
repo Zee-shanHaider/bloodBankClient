f9bb51aaee83a2951668c4e1f0c1f93bd39d475c
Zeeshan Haider <zeeshan9402529@gmail.com>
2022-11-30 11:42:02 +0500

    add modified file

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux"
 import { userReducer } from "./user/userReducer"
 import { donorReducer } from "./donor/donorReducer"
 export const rootReducer = combineReducers({
+    //checking pr
     user: userReducer,
     donor: donorReducer,
 }) 
